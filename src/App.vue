b3c441a6a87af45323855339468165b7df4708d9
Zeheng Li <imzehengl@gmail.com>
2020-02-18 13:28:04 -0700

    hide examples by default and some code clean up

--- src/App.vue
+++ src/App.vue
@@ -9,28 +9,18 @@
       class="flex mb-4 py-4 font-bold text-xl text-center"
       v-on:click="showExample = !showExample"
     >
-      <span class="flex items-center ml-auto mr-auto" v-if="showExample"
-        ><MinusCircleIcon />Examples</span
-      >
-      <span class="flex items-center ml-auto mr-auto" v-else
-        ><PlusCircleIcon />Examples</span
-      >
+      <span class="flex items-center ml-auto mr-auto" v-if="showExample">
+        <MinusCircleIcon />Examples
+      </span>
+      <span class="flex items-center ml-auto mr-auto" v-else>
+        <PlusCircleIcon />Examples
+      </span>
     </div>
     <div v-if="showExample">
-      <CheckWritingNumberApp initAmount="0" inputDisabled />
-      <CheckWritingNumberApp initAmount=".12" inputDisabled />
-      <CheckWritingNumberApp initAmount="1" inputDisabled />
-      <CheckWritingNumberApp initAmount="12.3" inputDisabled />
-      <CheckWritingNumberApp initAmount="99." inputDisabled />
-      <CheckWritingNumberApp initAmount="100" inputDisabled />
-      <CheckWritingNumberApp initAmount="123.45" inputDisabled />
-      <CheckWritingNumberApp initAmount="999" inputDisabled />
-      <CheckWritingNumberApp initAmount="1000" inputDisabled />
-      <CheckWritingNumberApp initAmount="1999" inputDisabled />
-      <CheckWritingNumberApp initAmount="999999" inputDisabled />
-      <CheckWritingNumberApp initAmount="1000000999" inputDisabled />
       <CheckWritingNumberApp
-        initAmount="999999999999999999999999999.99"
+        v-for="(amount, index) in examples"
+        :key="index"
+        :initAmount="amount"
         inputDisabled
       />
     </div>
@@ -44,7 +34,24 @@ export default {
   name: "app",
   components: { CheckWritingNumberApp, MinusCircleIcon, PlusCircleIcon },
   data: function() {
-    return { showExample: true };
+    return {
+      showExample: false,
+      examples: [
+        "0",
+        ".12",
+        "1",
+        "12.3",
+        "99.",
+        "100",
+        "123.45",
+        "999",
+        "1000",
+        "1999",
+        "999999",
+        "1000000999",
+        "999999999999999999999999999.99"
+      ]
+    };
   }
 };
 </script>
