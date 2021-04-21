177b5eeaf360c6b306bccb02e5e1e2dd44ebb62d
Zeheng Li <imzehengl@gmail.com>
2021-04-20 23:39:48 -0600

    update some configs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/check-writing-number/" : "/"
+    process.env.NODE_ENV === "production" ? `/${process.env.BASE_URL}/` : "/"
 };
